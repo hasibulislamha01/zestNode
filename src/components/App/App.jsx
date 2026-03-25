43b91b45a2c98d1ef89146cfed8ab007f82232b9
hasibulislamha01 <hasibulislamha01@gmail.com>
2026-03-25 09:42:32 -0700

    handled search loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router';
 import AppCard from '../AppCard';
+import GlobalSpinner from '../GlobalSpinner';
 
 const notFound = "/assets/App-Error.png"
 
@@ -8,11 +9,14 @@ const App = () => {
     const apps = useLoaderData()
 
     const [filtered, setFiltered] = useState(apps)
+    const [loading, setLoading] = useState(false)
 
     const handleChange = (value) => {
+        setLoading(true)
         const query = value.toLowerCase()
         const filteredApps = apps?.filter(app => app.title.toLowerCase().includes(query))
         setFiltered(filteredApps)
+        setLoading(false)
     }
     return (
         <section className='container mx-auto'>
@@ -24,7 +28,7 @@ const App = () => {
             </div>
 
             <div className='flex justify-between font-semibold mb-5'>
-                <h4>Found {apps?.length} apps</h4>
+                <h4>Found {filtered?.length} apps</h4>
                 <input
                     className='input rounded-sm w-48 h-8'
                     type="text"
@@ -32,26 +36,31 @@ const App = () => {
                     onChange={(e) => handleChange(e.target.value)}
                 />
             </div>
-            <div>
-                {
-                    filtered?.length ?
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8 place-items-center'>
-                            {
-                                filtered?.map(app =>
-                                    <Link key={app.id} to={`/apps/${app?.id}`}>
-                                        <AppCard app={app} />
-                                    </Link>
-                                )
 
-                            }
-                        </div>
-                        :
-                        <div className='w-full flex flex-col items-center justify-center gap-10'>
-                            <img src={notFound} alt="app not found" />
-                            <h2 className='text-2xl font-medium text-purple-800'>Sorry, We could not found the app you are looking for. 😅</h2>
-                        </div>
-                }
-            </div>
+            {
+                !loading ?
+                    <div>
+                        {
+                            filtered?.length ?
+                                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-8 place-items-center'>
+                                    {
+                                        filtered?.map(app =>
+                                            <Link key={app.id} to={`/apps/${app?.id}`}>
+                                                <AppCard app={app} />
+                                            </Link>
+                                        )
+
+                                    }
+                                </div>
+                                :
+                                <div className='w-full flex flex-col items-center justify-center gap-10'>
+                                    <img src={notFound} alt="app not found" />
+                                    <h2 className='text-2xl font-medium text-purple-800'>Sorry, We could not found the app you are looking for. 😅</h2>
+                                </div>
+                        }
+                    </div> :
+                    <GlobalSpinner />
+            }
         </section>
     );
 };
